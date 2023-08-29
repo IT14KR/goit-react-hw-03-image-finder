e590e4d313b99fa4bfc05b9144c4cbfa93679cee
IT14KR <131267129+IT14KR@users.noreply.github.com>
2023-08-29 08:55:04 +0200

    changes

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -7,7 +7,11 @@ export const ImageGallery = ({ images, onClick }) => {
   return (
     <ImageGalleryUl>
       {images.map(image => (
-        <ImageGalleryItem key={image.id} image={image} onClick={onClick} />
+        <ImageGalleryItem
+          key={image.id}
+          image={image}
+          onClick={onClick}
+        />
       ))}
     </ImageGalleryUl>
   );
